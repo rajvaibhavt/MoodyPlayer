811c2c8f5d405ba12e00069678abc8318593f8fb
Raj Vaibhav <rajvt172@gmail.com>
2026-08-08 11:36:00 +0530

    Fix backend routing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
-require('dotenv').config();
+/*require('dotenv').config();
 const app = require('./src/app');
 const connectDb = require('./src/db/db');
+const PORT = process.env.PORT || 3000;
+connectDb()
+.then(() => {
+app.listen(PORT, () => {
+console.log(`Server is running on port ${PORT}`);
+});
+})
+.catch((err) => {
+console.error('Failed to connect to DB. Server not started.', err);
+process.exit(1);
+});
+*/
+require('dotenv').config();
+
+const app = require('./src/app');
+const connectDb = require('./src/db/db');
+
 const PORT = process.env.PORT || 3000;
 
 connectDb()
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const songRoutes = require('./routes/song.routes');
 const cors = require('cors');
 require('./db/db');
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('MoodyPlayer Backend is working!');
+});
+
 app.use('/', songRoutes);
 
-module.exports = app; 
+module.exports = app;
